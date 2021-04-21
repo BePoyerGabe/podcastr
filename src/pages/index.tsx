d6ce0a62359d1064c104f86092a04073b6e59d7b
BePoyerGabe <bernardopoyer00@gmail.com>
2021-04-20 22:31:18 -0300

    JsonServer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,23 @@
-
-export default function Home() {
+// Consumo de API
+// SPA - hook useEffect, fetch localhost:3333/episodes. Não é indexada
+// SSR - em qualquer page, exportar func. getServerSideProps, deve retornar um obj com props {}, toda vez q acessarem a Home
+// SSG - só renomear para getStaticProps e retornar um revalidate a mais, Gera um html estático para cada período. Funfa só em prod
+export default function Home(props) {
+  console.log(props.episodes)
   return (
     <h1>Index</h1>
   )
+}
+
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
